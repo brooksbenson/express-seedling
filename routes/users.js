dbe414cfac854ce735c2265ea7ecdbcbac5832de
unknown <brooksbenson01@gmail.com>
2018-10-08 15:33:13 -0600

    error handling, modified db connection

--- routes/users.js
+++ routes/users.js
@@ -1,34 +1,34 @@
-const express = require('express');
-const Joi = require('joi');
-const _ = require('lodash');
-const bcrypt = require('bcrypt');
-const { User, inputSchema } = require('../models/User');
-const auth = require('../middleware/auth');
-const admin = require('../middleware/admin');
-const debug = require('debug')('routers:users');
+const express = require("express");
+const Joi = require("joi");
+const _ = require("lodash");
+const bcrypt = require("bcrypt");
+const { User, inputSchema } = require("../models/User");
+const auth = require("../middleware/auth");
+const admin = require("../middleware/admin");
+const debug = require("debug")("routers:users");
 
 const router = express.Router();
 
-router.get('/current', auth, async (req, res) => {
+router.get("/current", auth, async (req, res) => {
   const { _id } = req.user;
-  const user = await User.findById(_id).select('-password');
+  const user = await User.findById(_id).select("-password");
 
   res.status(200).send(user);
 });
 
-router.get('/all', admin, async (req, res) => {
-  const users = await User.find().select('-password');
+router.get("/all", admin, async (req, res) => {
+  const users = await User.find().select("-password");
 
   res.status(200).send(users);
 });
 
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   const input = _.pick(req.body, Object.keys(inputSchema));
   await Joi.validate(input, inputSchema);
   const salt = await bcrypt.genSalt();
   const password = await bcrypt.hash(input.password, salt);
   const user = await new User({ ...input, password }).save();
-  res.status(200).send('user successfully created');
+  res.status(200).send("user successfully created");
 });
 
 module.exports = router;
--- startup/db.js
+++ startup/db.js
@@ -1,21 +1,23 @@
-const mongoose = require('mongoose');
-const config = require('config');
-const debug = require('debug')('startup:db');
-const Joi = require('joi');
-Joi.objectid = require('joi-objectid')(Joi);
+const mongoose = require("mongoose");
+const config = require("config");
+const debug = require("debug")("startup:db");
+const Joi = require("joi");
+Joi.objectid = require("joi-objectid")(Joi);
 
 module.exports.init = function() {
-  const connectionString = config.get('dbConnectionString');
-  if (!connectionString) {
+  const u = config.get("db_user");
+  const p = config.get("db_password");
+  if (!u || !p) {
     throw new Error(
-      'Environment binding "app_db_connection_string" was not set'
+      "The database username and/or password was not set in the environment"
     );
   }
 
+  const connectionString = `mongodb://${u}:${p}@ds045107.mlab.com:45107/express-seed`;
   mongoose
     .connect(
       connectionString,
       { useNewUrlParser: true }
     )
-    .then(() => debug('db connection successful'));
+    .then(() => debug("db connection successful"));
 };
